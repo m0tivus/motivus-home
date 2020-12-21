fb26fbcf0372c3a7f6b72b0670b9a17c98ca8066
Cristian Huijse <cristian.huijse@gmail.com>
2020-12-21 09:19:23 -0300

    SE/CH Blog blockConetent to web blog

--- src/templates/article.js
+++ src/templates/article.js
@@ -2,46 +2,53 @@ import React from 'react'
 import { Link, graphql } from 'gatsby'
 import Img from 'gatsby-image'
 import Layout from '../components/layout' 
-import ReactMarkdown from "react-markdown"
+import PortableText from '../components/PortableText'
 import SEO from '../components/seo'
+import { Box, Grid, Typography } from '@material-ui/core'
+import "../components/layout.css"
+import { makeStyles } from '@material-ui/core/styles';
 
+const useStyles = makeStyles((theme) => ({
+  header:{
+    background: 'white',
+    zIndex: -100,
+    
+  }
+  
+}));
 
-
-const ArticleTemplate = ({ data, ...props}) => (
+export default function AricleTemplate ({data, ...props}) {
+  const classes = useStyles();
+  
+  return(
   <Layout { ...props}>
     <SEO title={data.sanityPost.title} description={data.sanityPost.abstract}/>
-    <h1>{data.sanityPost.title}</h1>
     
-    <p>by <Link to={`/authors/User_${data.sanityPost.author.id}`}>{data.sanityPost.author.name}</Link></p>
-    <Img fixed={data.sanityPost.image.asset.fixed}/>
-    <ReactMarkdown
-      escapeHtml={false}
-      source={data.sanityPost.content}
-      transformImageUri={uri => uri.startsWith('http') ? uri : `${process.env.IMAGE_BASE_URL}${uri}`}
-    />
+    <Box  display='flex' width='100%' justifyContent="center"  >
+      <Box  pl={5} pb={2} pt={2}  display='flex' width='90%' className={classes.header} boxShadow={2}>
+          <Grid container>
+            <Grid item xs={12}>
+              <Typography variant="h3" color="primary" bottomgutter>
+                {data.sanityPost.title}
+                </Typography> 
+              </Grid>
+              <Grid item xs={12}>
+                <Link to={`/authors/User_${data.sanityPost.author.id}`}>{data.sanityPost.author.name}</Link>
+              </Grid>
+            </Grid>
+      </Box>
+    </Box>
+    <Box display="flex" width='100%'justifyContent="center" boxShadow={6}>
+      <Img  fixed={data.sanityPost.image.asset.fixed}/>
+    </Box>
+    <Box display='flex' width='100%' justifyContent="center">
+      <Box p={5} display='flex' width='90%' justifyContent="center" boxShadow={2} >
+        {data.sanityPost._rawContent && <PortableText blocks={data.sanityPost._rawContent} />}  
+      </Box>
+    </Box>
   </Layout>
-)
+  ) 
+}
 
-export default ArticleTemplate
 
-export const query = graphql`
-  query ArticleTemplate($id: String!) {
-    sanityPost(id: {eq: $id}) {
-      title
-      content
-      abstract 
-      image {
-          asset {
-            fixed(width: 200, height: 125) {
-              ...GatsbySanityImageFixed
-            }
-          }
-        }
-      author {
-        id
-        name
-        
-      }
-    }
-  }
-`
+
