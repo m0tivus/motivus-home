216156719a428bdf6f504f615f1578f35d29dc34
Cristian Huijse <cristian.huijse@gmail.com>
2020-12-30 15:25:41 -0300

    SE/RI/FM/CH: images problem, colaboration and result

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,24 +1,27 @@
-const path = require(`path`);
+const path = require('path')
 
-const makeRequest = (graphql, request) => new Promise((resolve, reject) => {
-  // Query for nodes to use in creating pages.
-  resolve(
-    graphql(request).then(result => {
-      if (result.errors) {
-        reject(result.errors)
-      }
-      
-      return result;
-    })
-  )
-});
+const makeRequest = (graphql, request) =>
+  new Promise((resolve, reject) => {
+    // Query for nodes to use in creating pages.
+    resolve(
+      graphql(request).then((result) => {
+        if (result.errors) {
+          reject(result.errors)
+        }
+
+        return result
+      }),
+    )
+  })
 
 // Implement the Gatsby API “createPages”. This is called once the
 // data layer is bootstrapped to let plugins create pages from data.
 exports.createPages = ({ actions, graphql }) => {
-  const { createPage } = actions;
-  
-  const getArticles = makeRequest(graphql, `
+  const { createPage } = actions
+
+  const getArticles = makeRequest(
+    graphql,
+    `
     {
       allSanityPost {
         edges {
@@ -31,20 +34,20 @@ exports.createPages = ({ actions, graphql }) => {
         }
       }
     }
-    `).then(result => {
+    `,
+  ).then((result) => {
     // Create pages for each article.
     result.data.allSanityPost.edges.forEach(({ node }) => {
       createPage({
         path: `blog/${node.slug.current}`,
-        component: path.resolve(`src/templates/article.js`),
+        component: path.resolve('src/templates/article.js'),
         context: {
           id: node.id,
-
         },
       })
     })
-  });
-  
+  })
+
   // Query for articles nodes to use in creating pages.
-  return getArticles;
-};
+  return getArticles
+}
