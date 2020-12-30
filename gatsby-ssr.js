e37513bf5b1d8677f195021d9494917c86d783b7
Fernando Mora <f.mora.g90@gmail.com>
2020-12-30 15:13:47 -0300

    CH/SE/FM Update widget location

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -11,13 +11,13 @@ const React = require('react')
 exports.onRenderBody = ({ setHeadComponents }) => {
   setHeadComponents([
     <React.Fragment>
+      <script src='https://widget.motivus.cl/loader.js' async></script>
       <script
-        src='https://motivus-webpage.s3.amazonaws.com/widgetLoader.js'
-        async
+        dangerouslySetInnerHTML={{
+          __html:
+            'var Motivus = window.Motivus || {}; Motivus.client_id = \'1234\';',
+        }}
       ></script>
-      <script>
-        var Motivus = window.Motivus || {}; Motivus.client_id = '1234';
-      </script>
     </React.Fragment>,
   ])
 }
