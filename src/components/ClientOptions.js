63e2d6578e2699ca4f4fed3d9ef3223cf344bace
tek <setchegaray@gmail.com>
2021-02-11 19:50:24 -0300

    CH/RI/SE: add new video

--- src/components/ClientOptions.js
+++ src/components/ClientOptions.js
@@ -16,7 +16,7 @@ import Theme2 from './StyleTheme'
 import { FormatItalic, PlayCircleFilledWhite } from '@material-ui/icons'
 import ContactToggle from '../contexts/ContactToggle'
 import { navigate } from 'gatsby'
-//import ReactPlayer from 'react-player/youtube'
+import ReactPlayer from 'react-player/youtube'
 
 import sharePower from '../../static/sharePower.svg'
 import scientist from '../../static/scientist.svg'
@@ -163,19 +163,21 @@ export default function Intro() {
           With our framework you can process big volumes of data, and also earn
           extra income by renting your computer power to process data.
         </Typography>
-        {/*<Box justifyContent='center' display='flex'>
-          <ReactPlayer
-            url='https://youtu.be/IKHK4zPUCd0'
-            width='854px'
-            height='480px'
-            controls
-            config={{
-              youtube: {
-                embedOptions: { controls: 1 },
-              },
-            }}
-          />
-          </Box>*/}
+        {
+          <Box justifyContent='center' display='flex'>
+            <ReactPlayer
+              url='https://youtu.be/ORtNQkNx6kQ'
+              width='854px'
+              height='480px'
+              controls
+              config={{
+                youtube: {
+                  embedOptions: { controls: 1 },
+                },
+              }}
+            />
+          </Box>
+        }
       </Container>
       {/* End hero unit */}
       <Container maxWidth='lg' component='main'>
