77c577436d8d69040cff37d207d294a15c34f0a8
Fernando Mora <f.mora.g90@gmail.com>
2021-02-12 16:16:56 -0300

    RI|SE|FM Hotfix: smaller og:image

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ import HowItWorks from '../components/HowItWorksStep'
 const IndexPage = (props) => (
   <div>
     <Layout {...props}>
-      <SEO title='Home' image='/icons/icon-512x512.png' />
+      <SEO title='Home' image='/icons/icon-192x192.png' />
       <Intro />
       <HowItWorks></HowItWorks>
       <Features></Features>
