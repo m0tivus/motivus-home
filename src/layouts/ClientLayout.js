b67eace4380a0e40c3fd3e985e34961fa383c0c1
Cristian Huijse <cristian.huijse@gmail.com>
2021-07-16 13:16:20 -0400

    SE|CH|FM: initialize

---
+++ src/layouts/ClientLayout.js
@@ -0,0 +1,69 @@
+/**
+ * Layout component that queries for data
+ * with Gatsby's useStaticQuery component
+ *
+ * See: https://www.gatsbyjs.com/docs/use-static-query/
+ */
+
+import React from 'react'
+import PropTypes from 'prop-types'
+import { useStaticQuery, graphql } from 'gatsby'
+import {
+  createMuiTheme,
+  makeStyles,
+  ThemeProvider,
+  useTheme,
+} from '@material-ui/core/styles'
+import useMediaQuery from '@material-ui/core/useMediaQuery'
+
+//import Header from './header'
+//import Nav from './nav'
+//import MobileNav from './mobileNav'
+//import Footer from './Footer'
+import DarkTheme from './DarkTheme'
+//import SocialMedia from './SocialMedia'
+//import { set } from 'lodash'
+//import ContactToggle from '../contexts/ContactToggle'
+import { SnackbarProvider } from 'notistack'
+import { Box } from '@material-ui/core'
+import CssBaseline from '@material-ui/core/CssBaseline'
+
+const useStyles = makeStyles((theme) => ({
+  content: {
+    [theme.breakpoints.down('md')]: {
+      padding: theme.spacing(8, 0, 6),
+    },
+  },
+}))
+const Layout = ({ children, ...props }) => {
+  const [openContact, setOpenContact] = React.useState(false)
+
+  const theme = useTheme()
+  const classes = useStyles()
+  const matches = useMediaQuery(theme.breakpoints.down('md'))
+
+  return (
+    <SnackbarProvider
+      classes={{ variantInfo: { backgroundColor: theme.palette.primary.main } }}
+    >
+      <DarkTheme>
+        <CssBaseline></CssBaseline>
+        <Box>
+          <div
+            style={{
+              margin: '0 auto',
+            }}
+          >
+            <main className={classes.content}>{children}</main>
+          </div>
+        </Box>
+      </DarkTheme>
+    </SnackbarProvider>
+  )
+}
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
---
+++ src/layouts/DarkTheme.js
@@ -0,0 +1,147 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles'
+import { dark } from '@material-ui/core/styles/createPalette'
+
+const theme2 = createMuiTheme({
+  typography: {
+    fontFamily: 'Asap',
+
+    h1: {
+      fontWeight: '700',
+      fontStyle: 'italic',
+      fontSize: '5rem',
+      lineHeight: 1.334,
+      letterSpacing: '0em',
+    },
+
+    h2: {
+      fontWeight: '600',
+      fontStyle: 'italic',
+      fontSize: '3rem',
+      lineHeight: 1.2,
+      letterSpacing: '0em',
+    },
+
+    h3: {
+      fontWeight: '500',
+      fontStyle: 'italic',
+      fontSize: '2.5rem',
+      lineHeight: 1.167,
+      letterSpacing: '0em',
+    },
+
+    h4: {
+      fontWeight: '400',
+      fontStyle: 'italic',
+      fontSize: '1.85rem',
+      lineHeight: 1.235,
+      letterSpacing: '0em',
+    },
+
+    h5: {
+      fontWeight: '600',
+      fontFamily: 'Roboto',
+      fontSize: '1.2rem',
+      lineHeight: 1.334,
+      letterSpacing: '0em',
+    },
+
+    h6: {
+      fontWeight: '500',
+      fontFamily: 'Roboto',
+      fontSize: '1rem',
+      lineHeight: 1.25,
+      letterSpacing: '0em',
+    },
+
+    subtitle1: {
+      fontWeight: '400',
+      fontFamily: 'Roboto',
+      fontSize: '1rem',
+      lineHeight: 1.75,
+      letterSpacing: '0em',
+    },
+
+    subtitle2: {
+      fontWeight: '400',
+      fontFamily: 'Roboto',
+      fontSize: '0.875rem',
+      lineHeight: 1.58,
+      letterSpacing: '0em',
+    },
+
+    body1: {
+      fontWeight: '400',
+      fontFamily: 'Roboto',
+      fontSize: '1rem',
+      lineHeight: 1.5,
+      letterSpacing: '0em',
+    },
+
+    body2: {
+      fontWeight: '300',
+      fontFamily: 'Roboto',
+      fontSize: '1rem',
+      lineHeight: 1.5,
+      letterSpacing: '0em',
+    },
+
+    caption: {
+      fontFamily: 'Roboto',
+      fontWeight: 300,
+      fontSize: '0.85rem',
+    },
+  },
+
+  palette: {
+    primary: {
+      light: '#9455fe',
+      main: '#131144',
+      dark: '#1a0098',
+      contrastText: '#fff',
+    },
+
+    secondary: {
+      light: '#ff8fff',
+      main: '#cc5de7',
+      dark: '#9828b4',
+      contrastText: '#fff',
+    },
+
+    background: {
+      paper: '#2C2771',
+      default: '#131144',
+    },
+
+    text: {
+      primary: '#000000',
+      secondary: '#424242',
+    },
+  },
+
+  overrides: {
+    MuiTypography: {
+      gutterBottom: {
+        marginBottom: 16,
+      },
+    },
+  },
+  MuiTab: {
+    // Name of the rule
+    textColorSecondary: {
+      // Some CSS
+      color: '#aaa',
+    },
+  },
+})
+
+const Theme2 = ({ children }) => {
+  return <ThemeProvider theme={theme2}>{children}</ThemeProvider>
+}
+
+Theme2.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Theme2
