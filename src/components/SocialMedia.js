709fa0b8591e628cbfb1e621ef50c3ef9661fab2
Cristian Huijse <cristian.huijse@gmail.com>
2022-02-03 16:01:12 -0300

    feat(discord app): discord icon and link
    discord icon and link in social media

--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -1,13 +1,23 @@
 import { Box, IconButton } from '@material-ui/core'
 import { Facebook, GitHub, LinkedIn, Twitter } from '@material-ui/icons'
 import { navigate } from 'gatsby'
-
+import { makeStyles, useTheme } from '@material-ui/core/styles'
 import React from 'react'
 import logo from '../../static/logoVerticalBeta.svg'
 import benchmark from '../../static/benchmarkResult.svg'
 import { benchmarkAnimation } from '../styles/benchmarkAnimation.module.css'
+import { Icon } from '@iconify/react'
+
+const useStyles = makeStyles((theme) => ({
+  discordButton: {
+    outline: 'none',
+    border: '0px',
+    background: 'transparent',
+  },
+}))
 
 export default function SocialMedia() {
+  const classes = useStyles()
   return (
     <Box
       display={['none', 'none', 'none', 'flex']}
@@ -24,6 +34,7 @@ export default function SocialMedia() {
       <Box className={benchmarkAnimation} boxShadow={0}>
         <img alt='benchmarkStatus' src={benchmark} width='100px'></img>
       </Box>
+
       <Box
         position='fixed'
         top='7.5%'
@@ -32,6 +43,9 @@ export default function SocialMedia() {
       >
         <img alt='logoMotivus' src={logo} height='200px'></img>
       </Box>
+      <IconButton onClick={() => window.open('https://discord.gg/nesyJfA4')}>
+        <Icon icon='mdi:discord' color='#cc5de7' width='36' height='36' />
+      </IconButton>
       {
         <Box>
           <IconButton
