41cb9999ee3affa517977572724500e11983dfc6
Cristian Huijse <cristian.huijse@gmail.com>
2022-03-08 16:32:10 -0300

    fix(loading): when refresh appear home and not account/

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,12 +8,13 @@ import HowItWorks from '../components/HowItWorksStep'
 import Intro from '../components/Intro'
 import { Box } from '@material-ui/core'
 import Loading from '../components/Loading'
+;/^\/account/.test('/account/;lasdasdas')
 
 const IndexPage = (props) => (
   <div>
     <Layout {...props}>
       <SEO title='Home' image='https://motivus.cl/icons/icon-256x256.png' />
-      {/*<Loading fullscreen />*/}
+      {/^\/account/.test(props.location.pathname) && <Loading fullscreen />}
       <Intro />
       <Box mt='80px'>
         <CLientOptions />
