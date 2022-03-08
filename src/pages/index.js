1f8d186610b81d688ec6f8c8bf979d104ccaf181
Cristian Huijse <cristian.huijse@gmail.com>
2022-03-08 17:06:10 -0300

    fix(loading): loding account index problem WIP

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,21 +8,26 @@ import HowItWorks from '../components/HowItWorksStep'
 import Intro from '../components/Intro'
 import { Box } from '@material-ui/core'
 import Loading from '../components/Loading'
-;/^\/account/.test('/account/;lasdasdas')
 
-const IndexPage = (props) => (
-  <div>
-    <Layout {...props}>
-      <SEO title='Home' image='https://motivus.cl/icons/icon-256x256.png' />
-      {/^\/account/.test(props.location.pathname) && <Loading fullscreen />}
-      <Intro />
-      <Box mt='80px'>
-        <CLientOptions />
-      </Box>
-      <HowItWorks></HowItWorks>
-      <Features></Features>
-    </Layout>
-  </div>
-)
+const IndexPage = (props) => {
+  console.log(
+    /^\/account\//.test(props.location.pathname),
+    props.location.pathname,
+  )
+  return (
+    <div>
+      <Layout {...props}>
+        <SEO title='Home' image='https://motivus.cl/icons/icon-256x256.png' />
+        {/^\/account\//.test(props.location.pathname) && <Loading fullscreen />}
+        <Intro />
+        <Box mt='80px'>
+          <CLientOptions />
+        </Box>
+        <HowItWorks></HowItWorks>
+        <Features></Features>
+      </Layout>
+    </div>
+  )
+}
 
 export default IndexPage
