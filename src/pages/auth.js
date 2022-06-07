234a604262dd13fabc946d2ca76e83eb373a18bd
Fernando Mora <f.mora.g90@gmail.com>
2022-06-07 13:45:43 -0400

    fix(login): extra debug

--- src/pages/auth.js
+++ src/pages/auth.js
@@ -1,15 +1,16 @@
+import React from 'react'
 import { navigate } from 'gatsby-link'
 import withLocation from '../hoc/withLocation'
 
 const isBrowser = typeof window !== 'undefined'
 
 function Auth({ search }) {
+  console.log('auth token setting', search.token)
   if (isBrowser) {
-    console.log('auth token setting', search.token)
     window.localStorage.setItem('token', search.token)
     navigate('/account/login')
   }
-  return null
+  return <React.Fragment>Please wait</React.Fragment>
 }
 
 export default withLocation(Auth)
