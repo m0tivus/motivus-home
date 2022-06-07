32ceca2d758f3039a48f6373d5dd5fbd1b91481f
Fernando Mora <f.mora.g90@gmail.com>
2022-06-06 20:17:24 -0400

    ci(cdk): migration to v2

--- deployment/lib/deployment-stack.ts
+++ deployment/lib/deployment-stack.ts
@@ -1,8 +1,9 @@
-import * as cdk from '@aws-cdk/core'
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
 import { StaticSite } from './static-site'
 
 export class DeploymentStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props)
 
     new StaticSite(this, 'motivus-home', {
--- deployment/lib/static-site.ts
+++ deployment/lib/static-site.ts
@@ -1,13 +1,25 @@
 #!/usr/bin/env node
-import { Construct } from '@aws-cdk/core'
-import { HttpsRedirect } from '@aws-cdk/aws-route53-patterns'
-import cloudfront = require('@aws-cdk/aws-cloudfront')
-import route53 = require('@aws-cdk/aws-route53')
-import s3 = require('@aws-cdk/aws-s3')
-import s3deploy = require('@aws-cdk/aws-s3-deployment')
-import acm = require('@aws-cdk/aws-certificatemanager')
-import cdk = require('@aws-cdk/core')
-import targets = require('@aws-cdk/aws-route53-targets/lib')
+// import { Construct } from '@aws-cdk/core'
+// import { HttpsRedirect } from '@aws-cdk/aws-route53-patterns'
+// import cloudfront = require('@aws-cdk/aws-cloudfront')
+// import route53 = require('@aws-cdk/aws-route53')
+// import s3 = require('@aws-cdk/aws-s3')
+// import s3deploy = require('@aws-cdk/aws-s3-deployment')
+// import acm = require('@aws-cdk/aws-certificatemanager')
+// import cdk = require('@aws-cdk/core')
+// import targets = require('@aws-cdk/aws-route53-targets/lib')
+import {Construct} from 'constructs';
+import {
+  aws_certificatemanager as acm,
+  aws_cloudfront as cloudfront,
+  aws_route53 as route53,
+  aws_route53_targets as targets,
+  aws_s3 as s3,
+  aws_s3_deployment as s3deploy,
+  aws_iam as iam,
+  aws_cloudfront_origins as cloudfront_origins
+} from 'aws-cdk-lib';
+import { CfnOutput, Duration, RemovalPolicy} from 'aws-cdk-lib';
 
 export interface StaticSiteProps {
   domainName: string
@@ -30,21 +42,32 @@ export class StaticSite extends Construct {
       domainName: props.domainName,
     })
     const siteDomain = props.siteSubDomain + '.' + props.domainName
-    new cdk.CfnOutput(this, 'Site', { value: 'https://' + siteDomain })
+    new CfnOutput(this, 'Site', {value: 'https://' + siteDomain})
+
+    const cloudfrontOAI = new cloudfront.OriginAccessIdentity(this, 'cloudfront-OAI', {
+      comment: `OAI for ${name}`
+    });
 
     // Content bucket
     const siteBucket = new s3.Bucket(this, 'SiteBucket', {
       bucketName: props.domainName,
-      websiteIndexDocument: 'index.html',
-      websiteErrorDocument: 'index.html',
-      publicReadAccess: true,
-
-      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      publicReadAccess: false,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      removalPolicy: RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
     })
-    new cdk.CfnOutput(this, 'Bucket', { value: siteBucket.bucketName })
+
+    // Grant access to cloudfront
+    siteBucket.addToResourcePolicy(new iam.PolicyStatement({
+      actions: ['s3:GetObject'],
+      resources: [siteBucket.arnForObjects('*')],
+      principals: [new iam.CanonicalUserPrincipal(cloudfrontOAI.cloudFrontOriginAccessIdentityS3CanonicalUserId)]
+    }));
+
+    new CfnOutput(this, 'Bucket', {value: siteBucket.bucketName})
 
     // TLS certificate
-    const certificateArn = new acm.DnsValidatedCertificate(
+    const certificate = new acm.DnsValidatedCertificate(
       this,
       'SiteCertificate',
       {
@@ -53,32 +76,32 @@ export class StaticSite extends Construct {
         region: 'us-east-1',
         subjectAlternativeNames: [siteDomain],
       },
-    ).certificateArn
-    new cdk.CfnOutput(this, 'Certificate', { value: certificateArn })
-
-    // CloudFront distribution that provides HTTPS
-    const distribution = new cloudfront.CloudFrontWebDistribution(
-      this,
-      'SiteDistribution',
-      {
-        aliasConfiguration: {
-          acmCertRef: certificateArn,
-          names: [props.domainName, siteDomain],
-          sslMethod: cloudfront.SSLMethod.SNI,
-          securityPolicy: cloudfront.SecurityPolicyProtocol.TLS_V1_1_2016,
-        },
-        originConfigs: [
-          {
-            customOriginSource: {
-              domainName: siteBucket.bucketWebsiteDomainName,
-              originProtocolPolicy: cloudfront.OriginProtocolPolicy.HTTP_ONLY,
-            },
-            behaviors: [{ isDefaultBehavior: true }],
-          },
-        ],
-      },
     )
-    new cdk.CfnOutput(this, 'DistributionId', {
+    new CfnOutput(this, 'Certificate', {value: certificate.certificateArn})
+
+
+    // CloudFront distribution
+    const distribution = new cloudfront.Distribution(this, 'SiteDistribution', {
+      certificate: certificate,
+      defaultRootObject: "index.html",
+      domainNames: [siteDomain],
+      minimumProtocolVersion: cloudfront.SecurityPolicyProtocol.TLS_V1_2_2021,
+      errorResponses:[
+        {
+          httpStatus: 200,
+          responseHttpStatus: 200,
+          responsePagePath: '/index.html',
+          ttl: Duration.minutes(30),
+        }
+      ],
+      defaultBehavior: {
+        origin: new cloudfront_origins.S3Origin(siteBucket, {originAccessIdentity: cloudfrontOAI}),
+        compress: true,
+        allowedMethods: cloudfront.AllowedMethods.ALLOW_GET_HEAD_OPTIONS,
+        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+      }
+    })
+    new CfnOutput(this, 'DistributionId', {
       value: distribution.distributionId,
     })
 
@@ -96,12 +119,6 @@ export class StaticSite extends Construct {
       recordName: siteDomain,
       zone,
     })
-
-    // new HttpsRedirect(this, 'Redirect', {
-    //   zone,
-    //   recordNames: [siteDomain],
-    //   targetDomain: props.domainName,
-    // })
 
     // Deploy site contents to S3 bucket
     new s3deploy.BucketDeployment(this, 'DeployWithInvalidation', {
